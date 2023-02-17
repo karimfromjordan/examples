337adc1aa74e351d9bd502370cef443f69a00b18
Karim Jordan <karim@winter-rose.design>
2023-02-17 20:41:57 +0000

    POST test

---
+++ svelte-kit-test/src/routes/+page.server.js
@@ -0,0 +1,10 @@
+/** @type {import('./$types').Actions} */
+export const actions = {
+  async create(event) {
+    const data = await event.request.formData();
+
+    const text = data.get('text');
+
+    console.log(text);
+  }
+};
---
+++ svelte-kit-test/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { enhance } from "$app/forms";
+
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<form method="POST" action="?/create" enctype="application/x-www-form-urlencoded" use:enhance>
+  <div>
+    <label for="text">Input some text</label>
+    <input type="text" name="text" required>
+  </div>
+  <button>Submit</button>
+</form>
